6f472c0d05c24a7e73043234ededf789e00011fc
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-07-15 15:39:22 -0400

    port 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const next = require("next");
-const port = 8081;
+const port = 8080;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
