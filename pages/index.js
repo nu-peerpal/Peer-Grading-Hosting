749ccb874fdfa0e6866fc4702c7c5cb4a47e836b
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-07-15 13:37:49 -0400

    second

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -10,7 +10,8 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to JENNNA'S EPIC NEXT APP{" "}
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
@@ -54,7 +55,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +206,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
