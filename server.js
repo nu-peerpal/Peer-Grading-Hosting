e687942aa27bec0bbe1c2d4087dd0d1ac9a2dae6
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-07-15 17:00:46 -0400

    deploy to package.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const next = require("next");
 const port = 8080;
+//const port = process.env.PORT || 8081;
+
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
