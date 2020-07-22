7cc76e4a8a5271819604b3e2f7bec6ae8e50d8b6
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-07-22 10:38:19 -0400

    enrollment create working

--- pages/api/enrollment.js
+++ pages/api/enrollment.js
@@ -3,33 +3,29 @@ const Enrollment = db.enrollment;
 const Op = db.Sequelize.Op;
 
 export default (req, res) => {
-    // Validate request
-    if (!req.body.title) {
-        res.status(400).send({
-          message: "Content can not be empty!",
-        });
-        return;
-      }
-      // Create an enrollment
-      const enrollment = {
-        title: req.body.title,
-        description: req.body.description,
-        // published: req.body.published ? req.body.published : false
-      };
-      // Save enrollment in the database
-      Enrollment.create(enrollment)
-        .then((data) => {
-          res.send(data);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while creating the enrollment.",
-          });
-        });
+  // Validate request
+  if (!req.body.title) {
+    console.log(req);
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
   }
-
-// Create and Save new enrollment information
-exports.create = (req, res) => {
-    
+  // Create an enrollment
+  const enrollment = {
+    title: req.body.title,
+    description: req.body.description,
+    // published: req.body.published ? req.body.published : false
   };
+  // Save enrollment in the database
+  Enrollment.create(enrollment)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the enrollment.",
+      });
+    });
+};
