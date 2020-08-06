d48e898cb1ef1f1340ca99927c8a5fd9cd79e264
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-08-06 11:21:11 -0500

    another heroku attempt

--- config/index.js
+++ config/index.js
@@ -1,4 +1,2 @@
 const dev = process.env.NODE_ENV !== "production";
-export const server = dev
-  ? "http://localhost:8080"
-  : "http://pganextapp-env.eba-aveqnbwr.us-east-2.elasticbeanstalk.com";
+export const server = dev ? "http://localhost:8080" : "http://localhost:8080";
