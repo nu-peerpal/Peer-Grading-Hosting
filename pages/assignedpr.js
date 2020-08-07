32060bd920527c702d14b9e0d27256c58f7704e5
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-08-07 10:05:53 -0500

    capitals

--- pages/assignedpr.js
+++ pages/assignedpr.js
@@ -1,33 +1,29 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles/assignedpr.module.css";
-import ListContainer from '../components/listcontainer';
+import ListContainer from "../components/listcontainer";
 
 class assignedPRS extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
-      Current: [
-        { 'name': 'Assignment 3: Online Learning', 'info': "Due 1/12" },
-      ],
+      Current: [{ name: "Assignment 3: Online Learning", info: "Due 1/12" }],
       Past: [
-        { 'name': 'Assignment 1', 'info': "Graded" },
-        { 'name': 'Assignment 2', 'info': "" },
+        { name: "Assignment 1", info: "Graded" },
+        { name: "Assignment 2", info: "" },
       ],
-    }
+    };
   }
   render() {
-    const Current = this.state.Current
-    const Past = this.state.Past
-    
+    const Current = this.state.Current;
+    const Past = this.state.Past;
+
     return (
       <div className="Content">
-        <ListContainer name="Current Peer Reviews" data={Current}/>
-        <ListContainer name="Past Peer Reviews" data={Past}/>
+        <ListContainer name="Current Peer Reviews" data={Current} />
+        <ListContainer name="Past Peer Reviews" data={Past} />
       </div>
-    )
+    );
   }
 }
-
 
 export default assignedPRS;
