a04c120884b2437c63cc1577bfe0eab5044eb65c
emilyweintraub <emilyweintraub2021@u.northwestern.edu>
2020-08-13 11:09:04 -0400

    --started a toggle button for user views

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,40 @@ import Header from '../components/header'
 import Navbar from '../components/navbar'
 import WorkingNav from '../components/workingnav'
 import GoogleFonts from "next-google-fonts";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+
 
 export default function MyApp({ Component, pageProps }) {
-  const student = false
+  var isstudent = true
+
+  // const handleUser = (event, view) => {
+  //   if (view === 'prof/ta') {
+  //     isstudent = false
+  //   }
+  //   else {
+  //     isstudent = true
+  //   }
+  //   console.log(isstudent);
+  // };
+
   return (
     <div>
-      <GoogleFonts href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+      <GoogleFonts href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <Header />
-      <Navbar ISstudent={student}/>
-      <Component ISstudent={student} {...pageProps} />
-      <WorkingNav ISstudent={student}/>
+      {/* <ToggleButtonGroup exclusive onChange={handleUser} aria-label="State">
+        <ToggleButton value="student" aria-label="student">
+          Student
+        </ToggleButton>
+        <ToggleButton value="prof/ta" aria-label="prof/ta">
+          Prof/TA
+        </ToggleButton>
+      </ToggleButtonGroup> */}
+      <Navbar ISstudent={isstudent} />
+      <Component ISstudent={isstudent} {...pageProps} />
+      <WorkingNav ISstudent={isstudent} />
     </div>
 
   )
-}
+};
+
