d55898f3bb7ef7b6b5e93c6a4ff437b3d4248c03
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-08-18 19:10:23 -0400

    stashing

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,50 @@
-
 import React from "react";
 import Link from "next/link";
 
 import styles from "./styles/dashboard.module.css";
 import ListContainer from "../components/listcontainer";
-import { peerMatch, ensureSufficientReviews, submissionReports, reviewReports } from "./api/AlgCalls.js";
-import useSWR from 'swr'
-import Announcements from '../server/models/Announcements';
+import {
+  peerMatch,
+  ensureSufficientReviews,
+  submissionReports,
+  reviewReports,
+} from "./api/AlgCalls.js";
+import useSWR from "swr";
+import Announcements from "../server/models/Announcements";
 
-const fetcher = url => fetch(url, { method: 'GET' }).then(r => r.json())
+const fetcher = (url) => fetch(url, { method: "GET" }).then((r) => r.json());
 
-const PRS = [{'name': 'Peer Review Assignment 3: Online Learning', 'info': "Due 1/12"}]
+const PRS = [
+  { name: "Peer Review Assignment 3: Online Learning", info: "Due 1/12" },
+];
 const LIST = [
-          {'name': 'Grade User 1s Submission', 'info': "Assignment 2: Bid Analysis"},
-          {'name': 'Grade User 2s Submission', 'info': "Assignment 2: Bid Analysis"},
-          {'name': 'Grade User 3s Submission', 'info': "Assignment 2: Bid Analysis"},
-          {'name': 'Peer Matching', 'info': "Assignment 4"}
-        ]
+  { name: "Grade User 1s Submission", info: "Assignment 2: Bid Analysis" },
+  { name: "Grade User 2s Submission", info: "Assignment 2: Bid Analysis" },
+  { name: "Grade User 3s Submission", info: "Assignment 2: Bid Analysis" },
+  { name: "Peer Matching", info: "Assignment 4" },
+];
 
 function Dashboard(ISstudent) {
   if (ISstudent.ISstudent == true) {
-    var announc = []
-    const { data: announcement } = useSWR('/api/announcements/?courseId=1', fetcher)
-    if(announcement){announcement.map(x => announc.push({name: x.announcement, info:""}))}
+    var announc = [];
+    const { data: announcement } = useSWR(
+      "/api/announcements/?courseId=1",
+      fetcher
+    );
+    if (announcement) {
+      announcement.map((x) => announc.push({ name: x.announcement, info: "" }));
+    }
     return (
       <div className="Content">
         <ListContainer name="Todos" data={PRS} student={ISstudent.ISstudent} />
-        <ListContainer name="Announcements" data={announc} student={ISstudent.ISstudent} />
+        <ListContainer
+          name="Announcements"
+          data={announc}
+          student={ISstudent.ISstudent}
+        />
       </div>
-    )
-  }
-  else if (ISstudent.ISstudent == false) {
+    );
+  } else if (ISstudent.ISstudent == false) {
     return (
       <div className="Content">
         <ListContainer name="Todos" data={LIST} student={ISstudent.ISstudent} />
@@ -39,7 +53,7 @@ function Dashboard(ISstudent) {
           data={[{ name: "View As", info: "VIEW" }]}
         />
       </div>
-    )
+    );
   }
 }
 
