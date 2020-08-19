f24256a7b5f6496e97f1a01872779c8d1308ea1a
emilyweintraub <emilyweintraub2021@u.northwestern.edu>
2020-08-19 16:49:44 -0400

    added in new api for dashboard and for graded prs/assignments

--- pages/grades.js
+++ pages/grades.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import styles from "./styles/grades.module.css";
 import ListContainer from '../components/listcontainer';
+import useSWR from 'swr'
 
-class Grades extends React.Component {
-  constructor(props) {
-    super(props);
-    //console.log(props)
-    this.state = {
-      
-      PR: [
-        { 'name': 'Peer Review: Assignment 1', 'info': "96/100" },
-      ],
-      Assign: [
-        { 'name': 'Assignment 1', 'info': "96/100" },
-      ],
-    }
+// class Grades extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     //console.log(props)
+//     this.state = {
+
+//       PR: [
+//         { 'name': 'Peer Review: Assignment 1', 'info': "96/100" },
+//       ],
+//       Assign: [
+//         { 'name': 'Assignment 1', 'info': "96/100" },
+//       ],
+//     }
+//   }
+//   render() {
+//     const PR = this.state.PR
+//     const Assign = this.state.Assign
+
+//     // console.log(this.state)
+//     return (
+//       <div className="Content">
+//         <ListContainer name="Graded Peer Reviews" data={PR} link="/grades/viewgrade"/>
+//         <ListContainer name="Graded Assignments" data={Assign} link="/grades/viewgrade"/>
+//       </div>
+//     )
+//   }
+// }
+const fetcher = url => fetch(url, { method: 'GET' }).then(r => r.json())
+
+function Grades() {
+  var PRS = []
+  var Assignments = []
+  const { data: all } = useSWR('/api/graded/all?courseId=1&userId=1', fetcher)
+  if (all) {
+    PRS.push(all.Graded_Peer_Reviews)
+    Assignments.push(all.Grade_Assignments)
   }
-  render() {
-    const PR = this.state.PR
-    const Assign = this.state.Assign
-    
-    // console.log(this.state)
-    return (
-      <div className="Content">
-        <ListContainer name="Graded Peer Reviews" data={PR} link="/grades/viewgrade"/>
-        <ListContainer name="Graded Assignments" data={Assign} link="/grades/viewgrade"/>
-      </div>
-    )
-  }
+  return (
+    <div className="Content">
+      <ListContainer name="Graded Peer Reviews" data={PRS} link="/grades/viewgrade" />
+      <ListContainer name="Graded Assignments" data={Assignments} link="/grades/viewgrade" />
+    </div>
+  )
+
 }
-
 
 export default Grades;
--- pages/index.js
+++ pages/index.js
@@ -9,34 +9,52 @@ import Announcements from '../server/models/Announcements';
 
 const fetcher = url => fetch(url, { method: 'GET' }).then(r => r.json())
 
-const PRS = [{'name': 'Peer Review Assignment 3: Online Learning', 'info': "Due 1/12"}]
+const PRS = [{ 'name': 'Peer Review Assignment 3: Online Learning', 'info': "Due 1/12" }]
 const LIST = [
-          {'name': 'Grade User 1s Submission', 'info': "Assignment 2: Bid Analysis"},
-          {'name': 'Grade User 2s Submission', 'info': "Assignment 2: Bid Analysis"},
-          {'name': 'Grade User 3s Submission', 'info': "Assignment 2: Bid Analysis"},
-          {'name': 'Peer Matching', 'info': "Assignment 4"}
-        ]
+  { 'name': 'Grade User 1s Submission', 'info': "Assignment 2: Bid Analysis" },
+  { 'name': 'Grade User 2s Submission', 'info': "Assignment 2: Bid Analysis" },
+  { 'name': 'Grade User 3s Submission', 'info': "Assignment 2: Bid Analysis" },
+  { 'name': 'Peer Matching', 'info': "Assignment 4" }
+]
 
 function Dashboard(ISstudent) {
   if (ISstudent.ISstudent == true) {
     var announc = []
+    var todoprs = []
     const { data: announcement } = useSWR('/api/announcements/?courseId=1', fetcher)
-    if(announcement){announcement.map(x => announc.push({name: x.announcement, info:"", data: x}))}
+    const { data: todo } = useSWR('/api/peerReviews/all?courseId=1&userId=1&current=1', fetcher)
+    console.log(todo)
+    if (announcement) {
+      announcement.map(x =>
+        announc.push(
+          { name: x.announcement, info: "", data: x }
+        )
+      )
+    }
+    if (todo) {
+      todo.map(x =>
+        todoprs.push(
+          { name: x.assignment.name, info: "", data: x.assignment.peerreviewDueDate }
+        )
+      )
+    }
+
+
     return (
       <div className="Content">
-        <ListContainer name="Todos" data={PRS} student={ISstudent.ISstudent} link="/peer_reviews/peerreview" />
-        <ListContainer name="Announcements" data={announc} student={ISstudent.ISstudent} link=""/> {/*link depends on the announcement*/}
+        <ListContainer name="Todos" data={todoprs} student={ISstudent.ISstudent} link="/peer_reviews/peerreview" />
+        <ListContainer name="Announcements" data={announc} student={ISstudent.ISstudent} link="" /> {/*link depends on the announcement*/}
       </div>
     )
   }
   else if (ISstudent.ISstudent == false) {
     return (
       <div className="Content">
-        <ListContainer name="Todos" data={LIST} student={ISstudent.ISstudent} link=""/> {/*link depends on the todo*/}
+        <ListContainer name="Todos" data={LIST} student={ISstudent.ISstudent} link="" /> {/*link depends on the todo*/}
         <ListContainer
           name="View As Student"
           data={[{ name: "View As", info: "VIEW" }]}
-          link="" 
+          link=""
         />{/*link needs to be figured out later, might always be blank*/}
       </div>
     )
