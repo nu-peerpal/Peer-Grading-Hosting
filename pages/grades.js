b200276889da86583b032ab837b3268ddbfc5a48
Andrew Chung <drew.c3071@gmail.com>
2020-11-17 12:31:15 +0900

    complete grades page with new endpoint

--- pages/grades.js
+++ pages/grades.js
@@ -1,54 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import styles from "./styles/grades.module.css";
-import ListContainer from '../components/listcontainer';
-import useSWR from 'swr'
+import ListContainer from "../components/listcontainer";
 
-// class Grades extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     //console.log(props)
-//     this.state = {
-
-//       PR: [
-//         { 'name': 'Peer Review: Assignment 1', 'info': "96/100" },
-//       ],
-//       Assign: [
-//         { 'name': 'Assignment 1', 'info': "96/100" },
-//       ],
-//     }
-//   }
-//   render() {
-//     const PR = this.state.PR
-//     const Assign = this.state.Assign
-
-//     // console.log(this.state)
-//     return (
-//       <div class="Content">
-//         <ListContainer name="Graded Peer Reviews" data={PR} link="/grades/viewprgrade"/>
-//         <ListContainer name="Graded Assignments" data={Assign} link="/grades/viewassignmentgrade"/>
-//       </div>
-//     )
-//   }
-// }
-const fetcher = url => fetch(url, { method: 'GET' }).then(r => r.json())
+const fetcher = url => fetch(url, { method: "GET" }).then(r => r.json());
 
 function Grades() {
-  var PRS = []
-  var Assignments = []
-  const { data: all } = useSWR('/api/student/graded/all?courseId=1&userId=1', fetcher)
-  if (all) {
-    // console.log('Ok', all.Graded_Peer_Reviews, all.Graded_Assignments)
-    all.Graded_Peer_Reviews.map(x => PRS.push({ name: x.name, info: x.review_grades[0].grade, data: x }))
-    all.Graded_Assignments.map(x => Assignments.push({ name: x.assignment.name, info: x.submission_grades[0].grade, data: x }))
-  }
-  // console.log('lets see', Assignments)
+  const [submissionGrades, setSubmissionGrades] = useState([]);
+  const [reviewGrades, setReviewGrades] = useState([]);
+  const userId = 1;
+
+  useEffect(() => {
+    (async () => {
+      let resData = await fetcher("/api/assignments?graded=true&courseId=1");
+      let assignments = resData.data;
+
+      let submissions = [];
+      let reviewGrades = [];
+
+      const submissionReviewGrades = await Promise.all(
+        assignments.map(async ({ id }) => {
+          const getSubmissions = async () => {
+            let resData = await fetcher(
+              `/api/groups?assignmentId=${id}&userId=${userId}`,
+            );
+            const groupId = resData.data[0].id; // user should only have one group for the assignment
+
+            resData = await fetcher(
+              `/api/submissions?assignmentId=${id}&groupId=${groupId}`,
+            );
+            return resData.data; // should only be 0 or 1 submission grade per assignment
+          };
+
+          const getReviewGrades = async () => {
+            let resData = await fetcher(
+              `/api/reviewGradesReports?assignmentId=${id}&userId=${userId}`,
+            );
+            return resData.data; // should only be 0 or 1 review grade per assignment
+          };
+
+          return Promise.all([getSubmissions(), getReviewGrades()]);
+        }),
+      );
+
+      for (const [submission, reviewGrade] in submissionReviewGrades) {
+        if (submission) {
+          // if submission grade exists
+          submissions = [...submissions, ...submission];
+        }
+        if (reviewGrade) {
+          // if review grade exists
+          reviewGrades = [...reviewGrades, ...reviewGrade];
+        }
+      }
+
+      submissions = submissions.map((submission, i) => ({
+        name: assignments[i].name,
+        info: submission.grade,
+        data: submission,
+      }));
+
+      reviewGrades = submissions.map((reviewGrade, i) => ({
+        name: assignments[i].name,
+        info: reviewGrade.grade,
+        data: reviewGrade,
+      }));
+
+      setSubmissionGrades(submissions);
+      setReviewGrades(reviewGrades);
+    })();
+  }, []);
+
   return (
     <div class="Content">
-      <ListContainer name="Graded Peer Reviews" data={PRS} link="/grades/viewprgrade" />
-      <ListContainer name="Graded Assignments" data={Assignments} link="/grades/viewassignmentgrade" />
+      <ListContainer
+        name="Graded Peer Reviews"
+        data={reviewGrades}
+        link="/grades/viewprgrade"
+      />
+      <ListContainer
+        name="Graded Assignments"
+        data={submissionGrades}
+        link="/grades/viewassignmentgrade"
+      />
     </div>
-  )
-
+  );
 }
 
 export default Grades;
