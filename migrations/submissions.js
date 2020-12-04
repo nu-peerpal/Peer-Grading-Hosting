bb8236bce3b0c2ca62823ca5ff04fcea5284fc0a
Andrew Chung <drew.c3071@gmail.com>
2020-12-04 14:29:29 +0900

    quick changes to viewassignments

--- migrations/submissions.js
+++ migrations/submissions.js
@@ -8,7 +8,18 @@ const postSubmissions = () => {
       assignmentId: 1,
       canvasId: 10,
       grade: -1,
-      report: {},
+      report: {
+        reportBody: `# Submission Report (Grade: 50)\n\n<submission id="1" />\n\n
+          |        Rubric        | TA Review 1 [[?]][1] | Peer Review 1 
+          [[?]][2] |\n| :------------------: | :------------------: 
+          | :------------------: | \n|       Content        |      
+          6 [[?]][3]      |      6 [[?]][4]      |\n|   Writing Quality    
+          |      4 [[?]][5]      |      4 [[?]][6]      |\n|        
+          Total         |          50          |          50          
+          |\n\n[1]: # "T\n​\n​r\n​\n​y\n​\n​ \n​\n​h\n​\n​a\n​\n​r\n​\n​d\n​\n​e\n​\n​r"\n[2]: 
+          # "T\n​\n​r\n​\n​y\n​\n​ \n​\n​h\n​\n​a\n​\n​r\n​\n​d\n​\n​e\n​\n​r"\n[3]: # "okay"
+          \n[4]: # "okay"\n[5]: # "bad"\n[6]: # "bad"`,
+      },
       s3Link: "",
       groupId: 1,
     },
