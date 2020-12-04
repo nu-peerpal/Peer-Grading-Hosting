8011c4f1cab730542289155a66ca52a5bfaf6910
Andrew Chung <drew.c3071@gmail.com>
2020-12-05 08:56:11 +0900

    quick fix to functional components

--- pages/assignments/submissionreportlist.js
+++ pages/assignments/submissionreportlist.js
@@ -1,23 +1,13 @@
 import React from "react";
-import Link from 'next/link'
 import "./submissionreportlist.module.css";
-import Container from '../../components/container'
+import Container from "../../components/container";
 
-class SubmissionReports extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      }
-    }
-    render() {
-    //   const = this.state
-    return (
-      <div className="Content">
-      <Container name="Submission Reports for Assignment 1"/>
-      </div>
-    )
-    }
-  }
-  
-  export default SubmissionReports;
-  
+const SubmissionReports = () => {
+  return (
+    <div className="Content">
+      <Container name="Submission Reports for Assignment 1" />
+    </div>
+  );
+};
+
+export default SubmissionReports;
