12a8b29560dc449f86339935b5cd671941709838
Andrew Chung <drew.c3071@gmail.com>
2020-12-06 06:19:45 +0900

    add necessary migrations for tagrading

--- migrations/peerMatchings.js
+++ migrations/peerMatchings.js
@@ -6,21 +6,37 @@ const postPeerMatchings = () => {
     {
       id: 1,
       matchingType: "initial",
-      review: {},
-      reviewReview: {},
+      review: {
+        scores: [
+          [
+            9,
+            "Great algorithm. Could use a little work on the implementation, but overall I feel like it's fine."
+          ],
+          [
+            8,
+            "Proof could be better, but took into consideration that it was a difficult question."
+          ],
+          [
+            8.5,
+            "Fairly clear: not much else to ask for, but it didn't surpass any expectations."
+          ]
+        ],
+        comments: ""
+      },
+      reviewReview: null,
       assignmentId: 1,
       submissionId: 1,
-      userId: 1,
+      userId: 1
     },
     {
       id: 2,
       matchingType: "initial",
-      review: {},
-      reviewReview: {},
+      review: null,
+      reviewReview: null,
       assignmentId: 1,
       submissionId: 2,
-      userId: 1,
-    },
+      userId: 1
+    }
   ];
   return axios.post(`${server}/api/peerReviews?type=multiple`, peerMatchings);
 };
