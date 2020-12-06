d1b7fd14394be0b6525840242263e6746aaef228
Andrew Chung <drew.c3071@gmail.com>
2020-12-07 04:36:57 +0900

    update announcements test

--- pages/api/__tests__/announcements.test.js
+++ pages/api/__tests__/announcements.test.js
@@ -9,46 +9,49 @@ let entries = {
       id: 1,
       active: true,
       canvasId: "1",
-      courseName: "CS 111",
-    },
+      courseName: "CS 111"
+    }
   ],
   announcements: [
     {
       id: 1,
       announcement: "Hello",
-      courseId: 1,
+      courseId: 1
     },
     {
       id: 2,
       announcement: "How are you",
-      courseId: 1,
-    },
-  ],
+      courseId: 1
+    }
+  ]
 };
 
 describe("announcement tests", () => {
-  beforeAll(async (done) => {
+  beforeAll(async done => {
     appClient = createTestClient(announcementsHandler);
     await db.connect();
     done();
   });
 
-  beforeEach(async (done) => {
+  beforeEach(async done => {
     await db.sequelize.sync({ force: true }); // drop then create all tables
     for (const table in entries) {
       const tableEntries = entries[table];
-      await Promise.all(tableEntries.map((entry) => db[table].create(entry)));
+      await Promise.all(tableEntries.map(entry => db[table].create(entry)));
     }
     done();
   });
 
-  it("should get a list of announcements", async (done) => {
+  it("should get a list of announcements", async done => {
     const res = await appClient.get("?courseId=1");
     expect(res.statusCode).toEqual(200);
-    const expectedRes = entries.announcements.map(({ announcement }) => ({
-      announcement,
-    }));
-    expect(res.body).toEqual(expectedRes);
+    expect(
+      res.body.data.map(({ id, announcement, courseId }) => ({
+        id,
+        announcement,
+        courseId
+      }))
+    ).toEqual(entries.announcements);
     done();
   });
 });
