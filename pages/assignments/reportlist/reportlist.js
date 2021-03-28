93088255c8db76e88ec769446fbbb3ce9aa8b22b
Bradley Ramos <bradleyramos2021@u.northwestern.edu>
2021-03-28 09:58:02 -0400

    run and load report algs

--- pages/assignments/reportlist/reportlist.js
+++ pages/assignments/reportlist/reportlist.js
@@ -5,189 +5,97 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from "@material-ui/core/Button";
 import styles from './reviewreportlist.module.scss';
 import subData from "../../../sample_data/submissionReports";
 import revData from "../../../sample_data/reviewReports";
-import { submissionReports } from "../../api/AlgCalls.js";
+import { submissionReports, reviewReports } from "../../api/AlgCalls.js";
+import { useUserData } from "../../../components/storeAPI";
+import { useRouter } from 'next/router'
 const ReactMarkdown = require('react-markdown');
 const gfm = require('remark-gfm')
+const canvasCalls = require("../../../canvasCalls");
 
-
-function formatData(data) {
-  var dataMap = new Map();
-
-  for (var review of data) {
-    // clarifying each representation
-    var user_id = review[0];
-    var submission_id = review[1];
-    var review_obj = review[2];
-
-    // if we haven't seen the user_id yet
-    if (!dataMap.has(user_id)) {
-      //add that user_id + array with submission_id and scores object
-      dataMap.set(user_id, [[submission_id, review_obj]])
-    }
-
-    // if we have seen the user_id
-    else {
-      var temp = dataMap.get(user_id);
-      temp.push([submission_id, review_obj]);
-      dataMap.set(user_id, temp)
-    }
-  }
-
-  // console.log(dataMap)
-  // var finalData = [];
-  // for (let [key, value] of dataMap) {
-  //   finalData.push([key, value]);
-  // }
-  return dataMap;
-}
-
-function FormatSection(data) {
-  var section = [];
-  for (var review of data) {
-    section.push(
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography >User ID: {review[0]}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            {review[1]}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    )
-  }
-  return section;
-}
 
 const ReviewReports = () => {
+  const router = useRouter()
+  const { userId, courseId, courseName, assignment, key, setKey } = useUserData();
+  const [needsLoading, setNeedsLoading] = useState(true);
   const [subReports, setSubReports] = useState([]);
+  const [revReports, setRevReports] = useState([]);
+  const { assignmentId, assignmentName } = router.query;
 
-  useEffect(() => {
-    submissionReports(
-      subData.graders,
-      subData.reviews,
-      subData.rubric
-    ).then(reports => {
+  function generateReports() {
+    Promise.all([submissionReports(subData.graders,subData.reviews,subData.rubric),reviewReports(revData.graders,revData.reviews,revData.rubric),canvasCalls.getUsers(canvasCalls.token,courseId)])
+    .then(reports => {
       console.log('reports',reports);
-      setSubReports(reports[1]);
+      setSubReports(reports[0][1]);
+      // Change User ID to User Name for Review Reports
+      let users = reports[2];
+      for (let revRep in reports[1][1]) {
+        let i = users.findIndex(x => x.canvasId == reports[1][1][revRep][0])
+        reports[1][1][revRep][0] = users[i]["firstName"] + " "+ users[i]["lastName"];
+      }
+      setRevReports(reports[1][1]);
+      setNeedsLoading(false);
+
     });
+  }
+  useEffect(() => {
     // console.log(reports);
     // submissionReports(subData.graders,subData.reviews,subData.rubric)
   },[])
-  
-  // var rev = (formatData(revData.reviews));
-  // console.log(rev)
-  // var keys = [...rev.keys()];
-  // console.log(rev.get(keys[0]))
+
   return (
-    <div className="Content">
-      <Container name="Review Reports for Assignment 1" >
+    <div className="Content"> 
+      {needsLoading ? 
+        <Container name={"Generate Reports"}>
+          <Button onClick={() => generateReports()}>Generate Reports</Button>
+        </Container>
+      :
+        <div>
+        <Container name={"Submission Reports for " + assignmentName} >
+          {
+            subReports.map(sub =>
+              <Accordion key={sub[0]}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                <Typography >Submission {sub[0]}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className={styles.details}>
+                      <ReactMarkdown plugins={[gfm]} children={sub[1]} />
+                    </div>
+                </AccordionDetails>
+              </Accordion>
+            )
+          }
+        </Container>
+        <Container name={"Review Reports for " + assignmentName}>
         {
-          subReports.map(sub =>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography >User ID: {sub[0]}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  <div className={styles.details}>
-                    <ReactMarkdown plugins={[gfm]} children={sub[1]} />
-                  </div>
-                  {/* <div className={styles.details}>
-                    <div className={styles.submissionContainer}>
-                    {
-                      rev.get(user_id).map(submission =>
-                        <div className={styles.submission}>
-                          <div className={styles.submission__title}>Submission #: {submission[0]}</div>
-                          <div className={styles.submission__body}>
-                            <div className={styles.scoreContainer}>
-                              {submission[1].scores.map(score =>
-                                <div className={styles.score}>
-                                  <p className={styles.score__rubric_field}>{revData.rubric[submission[1].scores.indexOf(score)][1]}</p>
-                                  <p className={styles.score__value}>{score[0]}</p>
-                                  <p className={styles.score__title}>{score[1]}</p>
-                                </div>
-                              )}
-                            </div>
-                            <div>
-                                <p className={styles.details__title}>Comments</p>
-                                {submission[1].comments}
-                            </div>
-                          </div>
-                        </div>
-                      )
-                    }
-                    </div> */}
-                    {/* <p className={styles.details__title}>Scores</p>
-                    <div className={styles.scoreContainer}>
-                      {review[2].scores.map(score =>
-                        <div className={styles.score}>
-                          <p className={styles.score__value}>{score[0]}</p>
-                          <p className={styles.score__title}>{score[1]}</p>
-                        </div>
-                      )}
+            revReports.map(rev =>
+              <Accordion key={rev[0]+rev[1]}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                <Typography >{rev[0]}: submission {rev[1]}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className={styles.details}>
+                      <ReactMarkdown plugins={[gfm]} children={rev[2]} />
                     </div>
-                    <div>
-                      <p className={styles.details__title}>Comments</p>
-                      {review[2].comments}
-                    </div> */}
-                  {/* </div> */}
-
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-
-          )
-        }
-        {/* {
-          revData.reviews.map(review =>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography >User ID: {review[0]}, Submission ID: {review[1]}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  <div className={styles.details}>
-                    <div>
-                      <p className={styles.details__title}>Scores</p>
-                      <div className={styles.scoreContainer}>
-                        {review[2].scores.map(score =>
-                          <div className={styles.score}>
-                            <p className={styles.score__value}>{score[0]}</p>
-                            <p className={styles.score__title}>{score[1]}</p>
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                    <div>
-                      <p className={styles.details__title}>Comments</p>
-                      {review[2].comments}
-                    </div>
-                  </div>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-
-          )
-        } */}
-      </Container>
-      <Container name="Submission Reports for Assignment 1" />
+                </AccordionDetails>
+              </Accordion>
+            )
+          }
+        </Container>
+        </div>
+      }
     </div>
   );
 };
