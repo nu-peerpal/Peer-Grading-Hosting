a6fdaea602e16c1f62b96cc13eeeb9033a26aee6
Bradley Ramos <bradleyramos2021@u.northwestern.edu>
2021-04-20 16:38:55 -0500

    in progress, completed assignments

--- pages/progressassignments.js
+++ pages/progressassignments.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ListContainer from "../components/listcontainer";
+import { useUserData } from "../components/storeAPI";
+const axios = require("axios");
 
 const InProgress = props => {
-  const progress = [
-    { name: "Assignment 2: Bid Analysis", info: "TA Grading" },
-    { name: "Assignment 3: Online Learning", info: "Peer Review Stage" },
-    { name: "Assignment 4", info: "Peer Matching Required" },
-  ];
+  const { userId, courseId } = useUserData();
+  const [assignments, setAssignments] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/assignments?courseId=${courseId}`).then(assignmentData => {
+      console.log({assignmentData});
+      setAssignments(assignmentData.data.data);
+    });
+  }, []);
+  // const progress = [
+  //   { name: "Assignment 2: Bid Analysis", info: "TA Grading" },
+  //   { name: "Assignment 3: Online Learning", info: "Peer Review Stage" },
+  //   { name: "Assignment 4", info: "Peer Matching Required" },
+  // ];
 
   return (
     <div className="Content">
       <ListContainer
-        name="Peer Assignments"
-        data={progress}
+        name="Peer Review Enabled Assignments"
+        data={assignments}
         link={"/assignments/fullassignmentview/fullassignmentview"}
       />
     </div>
