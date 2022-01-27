ff2459435ea504aa2b0d475d9830948f913dc9e5
Karthik Subramanian <thikkar@dhcp-10-105-222-37.wireless.northwestern.private>
2022-01-27 11:13:16 -0600

    UI for Downloading Data

--- pages/download/peerGradingData.js
+++ pages/download/peerGradingData.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from "../../components/container";
 import Link from 'next/link';
 import Button from "@material-ui/core/Button";
+import Router from "next/router";
+import downloadStyles from "./download.module.scss";
+import containerStyles from "../../components/styles/container.module.scss";
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 
 import { useUserData } from "../../components/storeAPI";
+// import Cookies from 'js-cookie';
 
 const axios = require("axios");
 
 const download = () => {
 
-  const { context_id } = useUserData();
-  //const { readFile, writeFile } = fs.promises;
-  console.log(context_id)
+  const { courseId } = useUserData();
+  const [ peerGradingData, setPeerGradingData ] = useState()
+  const tooltip = "Example tooltip to demonstrate how this feature would work"
+
+  // useEffect(() => {
+  //   if (Cookies.get('userData') && !savedStudentId) { // create new user if not viewing as student and cookie is set
+  //     console.log('creating user data');
+  //     const userData = JSON.parse(Cookies.get('userData'));
+  //     console.log({userData});
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    if (!courseId) {
+      console.log("waiting for courseId");
+      return;
+    }
+
+    axios.get(`/api/courseReviews/${courseId}`).then(data => {
+      setPeerGradingData(data)
+    });
+  }, [courseId]);
+
+  console.log(peerGradingData)
 
   const JSONToCSV = (jsonData) => {
     const data = jsonData.data.data;
     console.log(data);
-    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const replacer = (key, value) => value === null ? '' : value;
     const header = Object.keys(data[0]);
     const csv = [
         header.join(','), // header row first
@@ -28,54 +54,85 @@ const download = () => {
   }
 
   const handleJSONSubmit = () => {
-    axios.get(`/api/courseReviews/1`).then(peerGradingData => {
       const fileName = "PeerGradingData";
-      const json = JSON.stringify(peerGradingData);
+      console.log(peerGradingData.data.data)
+      const json = JSON.stringify(peerGradingData.data.data);
       const blob = new Blob([json],{type:'application/json'});
-      const href = URL.createObjectURL(blob); // Create a downloadable link
+      const href = URL.createObjectURL(blob);
       const link = document.createElement('a');       
       link.href = href;
       link.download = fileName + ".json";
-      document.body.appendChild(link);   // This can any part of your website
+      document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-    });
   }
 
   const handleCSVSubmit = () => {
-    axios.get(`/api/courseReviews/1`).then(peerGradingData => {
       const fileName = "PeerGradingData";
       const csv = JSONToCSV(peerGradingData);
       const blob = new Blob([csv],{type:'text/csv'});
-      const href = URL.createObjectURL(blob); // Create a downloadable link
+      const href = URL.createObjectURL(blob);
       const link = document.createElement('a');       
       link.href = href;
       link.download = fileName + ".csv";
-      document.body.appendChild(link);   // This can any part of your website
+      document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-    });
   }
 
+  // can easily switch line 86 to Container
   return (
-    <Container name={"Download Data"}>
-        <div>
-            <span>Download peer grading data as either JSON or .csv file below</span>
+    <div className={downloadStyles.containers}>
+      <KeyboardBackspaceIcon className={containerStyles.back} onClick={() => Router.back()} />
+
+      <h1><span>Download Peer Grading Data</span></h1>
+      
+      <div className={downloadStyles.buttonContainer}>
+        <div className={downloadStyles.button}>
+        
+            <Link href="#" passHref>
+              <Button onClick={handleJSONSubmit} 
+                      variant="contained" 
+                      color="primary"
+                      disabled={!peerGradingData}>
+                Download JSON
+              </Button>
+            </Link>
+
         </div>
+        <div className={downloadStyles.button}>
 
-        <Link href="#" passHref>
-          <Button onClick={handleJSONSubmit} variant="contained" color="primary">
-            Download JSON
-          </Button>
-        </Link>
+            <Link href="#" passHref>
+              <Button onClick={handleCSVSubmit} 
+                      variant="contained" 
+                      color="primary" 
+                      disabled={!peerGradingData}>
+                Download CSV
+              </Button>
+            </Link>
 
-        <Link href="#" passHref>
-          <Button onClick={handleCSVSubmit} variant="contained" color="primary">
-            Download .CSV
-          </Button>
-        </Link>
+        </div>
+      </div>
 
-    </Container>
+      {!peerGradingData 
+        ? <div className={downloadStyles.infoContainer}>
+            <h3>ERROR: CANNOT RETRIEVE DATA</h3>
+            <h5>(Please navigate back to 'Home' and try again)</h5>
+          </div>
+        : <div className={downloadStyles.infoContainer}>
+            <h2>Data Categories</h2>
+            <ul>
+              <li>review<span className={downloadStyles.tooltipText}>{tooltip}</span></li>
+              <li>reviewReview<span className={downloadStyles.tooltipText}>{tooltip}</span></li>
+              <li>assignmentId<span className={downloadStyles.tooltipText}>{tooltip}</span></li>
+              <li>userId<span className={downloadStyles.tooltipText}>{tooltip}</span></li>
+              <li>submissionId<span className={downloadStyles.tooltipText}>{tooltip}</span></li>
+              <li>matchingType<span className={downloadStyles.tooltipText}>{tooltip}</span></li>
+            </ul>
+          </div>
+        }
+
+    </div>
   );
 };
 
