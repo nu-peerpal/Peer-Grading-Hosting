88d0edf52e10b0e93eb545ade5f40062df8b3dd8
Karthik Subramanian <thikkar@dhcp-10-105-89-160.wireless.northwestern.private>
2022-04-11 14:14:02 -0500

    Added BarChart Component

---
+++ components/BarChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Bar } from "react-chartjs-2";
+
+const BarChart = ({ chartData, }) => {
+  return (
+    <Bar data={chartData} options={} />
+  )
+}
+
+export default BarChart
