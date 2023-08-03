4f93ab6b65d05b424452b0b6cd71837a77f26b8e
ndh4 <ndh4@rice.edu>
2023-08-02 21:43:39 -0500

    Fix studentResponse to return rejection

--- pages/api/utils/peerHandler.js
+++ pages/api/utils/peerHandler.js
@@ -2,7 +2,7 @@ const {response403} = require("./responseHandler");
 exports.studentRequest = async (req, res, handle) => {
     if (req.userData.student && req.userData.user_id !== req.query.userId) {
         console.log("Student attempt to act as another student: REJECTED");
-        response403(res);
+        return response403(res);
     }
     // We have ensured that either:
     // - The authenticated user is a non-student, or
